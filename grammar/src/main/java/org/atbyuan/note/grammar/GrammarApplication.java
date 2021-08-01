3a5dc73817e3586be47d915f617562bafa220261
atbyuan <atbyuan@outlook.com>
2021-08-02 01:04:34 +0800

    feat(atbyuan.note): grammar

--- grammar/src/main/java/org/atbyuan/note/grammar/GrammarApplication.java
+++ grammar/src/main/java/org/atbyuan/note/grammar/GrammarApplication.java
@@ -3,6 +3,11 @@ package org.atbyuan.note.grammar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author atbyuan
+ * @date 2021年8月1日 15点09分
+ **/
+
 @SpringBootApplication
 public class GrammarApplication {
 

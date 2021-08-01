ad7ef3b43f2f563e51c071574961e2a6402bde47
atbyuan <atbyuan@outlook.com>
2021-08-02 01:04:11 +0800

    feat(atbyuan.note): common

---
+++ common/src/main/java/org/atbyuan/note/entity/User.java
@@ -0,0 +1,22 @@
+package org.atbyuan.note.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author atbyuan
+ * @date 2021/8/1 15:47
+ **/
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer id;
+    private String mobile;
+    private String password;
+
+}
